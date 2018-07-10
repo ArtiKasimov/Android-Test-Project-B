a97ea1e4edf251fe28f27b94b006d99d3c0c4b82
Muliar Misha <muliar.misha@gmail.com>
2018-07-10 07:02:10 +0300

    load picrure more quickly

--- app/src/main/java/com/example/arturkasymov/application_b/ImageFragment.java
+++ app/src/main/java/com/example/arturkasymov/application_b/ImageFragment.java
@@ -13,8 +13,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.Toast;
-
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.RequestCreator;
@@ -79,22 +77,13 @@ public class ImageFragment extends Fragment {
                 if (!myDir.exists()) {
                     myDir.mkdirs();
                 }
-                v.postDelayed(new Runnable() {
-                    public void run() {
-                        loadPicture2();
-                    }
-                }, 500);
-
-
-
+                loadPicture2();
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
                         delete(id);
                     }
                 }).start();
-
-
             }
         }
         return v;
